83c7c8a62399e73ade7d65c0f6c228bc4d76339a
milan <melonchhetri@gmail.com>
2021-06-02 22:19:24 +0545

    dogPrediction is added

---
+++ dogUpload/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+
+# Create your views here.
+from dogUpload.forms import ImageUploadForm
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+
+def handle_uploaded_file(f):
+    with open('img.jpg','wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+def index(request):
+    return render(request,'index.html')
+def imageProcess(request):
+    form= ImageUploadForm(request.POST,request.FILES)
+    if form.is_valid():
+        handle_uploaded_file(request.FILES['image'])
+
+        model = ResNet50(weights='imagenet')
+
+        img_path = r'C:\Users\Milan\Pictures\cat.jpg'
+        img = image.load_img(img_path, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+
+        preds = model.predict(x)
+        print('Predicted:', decode_predictions(preds, top=3)[0])
+
+        html=decode_predictions(preds,top=3)[0]
+        res=[]
+        for e in html:
+            res.append((e[1],np.round(e[2]*100,2)))
+        return render(request,"result.html",{"res":res})
+
+
+    return render(request,'result.html')
